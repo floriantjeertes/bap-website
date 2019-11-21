820ca4a283f5dce1ac2cfce036cc05b21959ef84
florianmac <nairolf3>
2019-11-21 09:51:22 +0100

    update 21/11/19

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Florian
                 </div>
 
                 <div class="links">
