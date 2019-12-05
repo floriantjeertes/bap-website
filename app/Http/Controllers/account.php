4cb888f05154e249f935a34461c5a0a9b28c53a2
florianmac <nairolf3>
2019-12-05 10:15:17 +0100

    test

---
+++ app/Http/Controllers/account.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class account extends Controller
+{
+    public function acount(){
+        return "fuck  yeh het werkt";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,11 @@ Route::get('/dit/is/een/lange-url/zeg', 'PageController@longUrl');
 Route::redirect('/korteUrl', '/dit/is/een/lange-url/zeg');
 
 
-Route::get('/blog/artikel/{artikel}','BlogController@showArtikel');
+Route::get('/blog/artikel/{artikel}','BlogController@showArtikel')->where(['artikel' => '[a-z]+']);
 
+Route::prefix('mijn-account')->group(function (){
+    Route::get('instellingen', 'account@acount');
+    Route::get('bestellingen', 'account@acount');
 
+});
 
-//Route::get('/blog/artikel/{artikel}',function($artikel){
-//    return 'product '.$artikel;
-//});
