fb1681614cfe791c4507a9bd166ae6506f002e7f
florianmac <nairolf3>
2019-12-12 09:44:32 +0100

    test

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en"></html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=1.0,minimum-scale=1.0">
+    <meta http-equiv="x-ua-compatible" content="ie-edge">
+    <title>supah</title>
+
+
+    <link href="https://fonts.googleapis.com/css?family=Numit:200,600" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    HEADER
+</header>
+
+<nav>
+NAVIGATION
+</nav>
+
+<main>
+    MAIN
+</main>
+
+<footer>
+    FOOTER
+</footer>
+
+</body>
+</html>
